55401d89b3b61e32b525cb38caf17f1eba5de825
ronaldlussier83 <ronaldlussier83@gmail.com>
2022-10-03 01:30:50 -0400

    Version 1.0

--- src/secret.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Secrets:
-    sender_email: str = "rons.robots@gmail.com"
-    sender_password: str = "pdsngwgsupylfmot"
-    receiver_email: str = "ronaldlussier83@gmail.com, rons.robots@gmail.com"
